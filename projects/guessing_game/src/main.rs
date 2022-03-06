11de52d7e90ff9e17f4793d837b6e9fae5062d3c
Edmund <eyu1@ualberta.ca>
2022-03-05 19:38:48 -0700

    comment out guess, lower random number generation range

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -5,9 +5,9 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..11);
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
